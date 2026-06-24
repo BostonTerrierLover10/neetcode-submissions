cc72227fe42b7f5857482e6978fc3591f2a70fb7
Justin <162920841+BostonTerrierLover10@users.noreply.github.com>
2026-06-24 13:47:03 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,28 @@
+class MinStack {
+    private Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+       return stack.peek();
+    }
+    
+    public int getMin() {
+        int minimum = stack.peek();
+
+        for(int value : stack){
+            minimum = Math.min(minimum, value);
+        }
+        return minimum;
+    }
+}
